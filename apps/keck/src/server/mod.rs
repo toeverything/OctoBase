8afe727b6ac1d279815fa8e78e51bd2f0f02f915
DarkSky <darksky2048@gmail.com>
2022-09-07 13:54:05 +0800

    fix: listen on container

--- apps/keck/src/server/mod.rs
+++ apps/keck/src/server/mod.rs
@@ -16,7 +16,7 @@ pub async fn start_server() {
         )
         .fallback(get(files::handler));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
