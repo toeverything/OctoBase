22360090e613d6ef2313731fc24a500bd2fc3837
DarkSky <darksky2048@gmail.com>
2022-09-22 10:59:02 +0800

    test: add more assert

--- apps/keck/src/sync/storage.rs
+++ apps/keck/src/sync/storage.rs
@@ -7,7 +7,7 @@ use sqlx::{
 };
 use std::str::FromStr;
 
-#[derive(sqlx::FromRow, Debug)]
+#[derive(sqlx::FromRow, Debug, PartialEq)]
 pub struct UpdateBinary {
     id: i64,
     pub blob: Vec<u8>,
@@ -44,6 +44,13 @@ impl SQLite {
         Ok(ret.0)
     }
 
+    pub async fn max_id(&self) -> Result<i64, Error> {
+        let stmt = format!("SELECT max(id) FROM {}", self.table);
+        let mut conn = self.get_conn().await?;
+        let ret = query_as::<_, Count>(&stmt).fetch_one(&mut conn).await?;
+        Ok(ret.0)
+    }
+
     pub async fn insert(&self, blob: &[u8]) -> Result<(), Error> {
         let stmt = format!("INSERT INTO {} VALUES (null, ?);", self.table);
         let mut conn = self.get_conn().await?;
@@ -51,7 +58,7 @@ impl SQLite {
         Ok(())
     }
 
-    pub async fn delete(&self, idx: i64) -> Result<(), Error> {
+    pub async fn delete_before(&self, idx: i64) -> Result<(), Error> {
         let stmt = format!("DELETE FROM {} WHERE id < ?", self.table);
         let mut conn = self.get_conn().await?;
         query(&stmt).bind(idx).execute(&mut conn).await?;
@@ -101,21 +108,46 @@ pub async fn init<T: ToString>(conn: SqlitePool, table: T) -> Result<SQLite, Err
 }
 
 mod tests {
+    use super::*;
+
+    async fn init_memory_pool() -> anyhow::Result<SqlitePool> {
+        let path = format!("sqlite::memory:");
+        let options = SqliteConnectOptions::from_str(&path)?
+            .journal_mode(SqliteJournalMode::Wal)
+            .create_if_missing(true);
+        Ok(SqlitePool::connect_with(options).await?)
+    }
+
     #[tokio::test]
-    async fn sync_storage_test() -> anyhow::Result<()> {
-        let pool = super::init_pool("jwst").await?;
-        let sqlite = super::init(pool, "updates").await?;
+    async fn basic_storage_test() -> anyhow::Result<()> {
+        let pool = init_memory_pool().await?;
+        let sqlite = init(pool, "basic").await?;
 
         sqlite.insert(&[1, 2, 3, 4]).await?;
-        println!("count: {}", sqlite.count().await?);
+        assert_eq!(sqlite.count().await?, 1);
         sqlite.insert(&[2, 2, 3, 4]).await?;
-        sqlite.delete(2).await?;
-        println!("data: {:?}", sqlite.all(0).await?);
+        sqlite.delete_before(2).await?;
+        assert_eq!(
+            sqlite.all(0).await?,
+            vec![UpdateBinary {
+                id: 2,
+                blob: vec![2, 2, 3, 4]
+            }]
+        );
+        assert_eq!(sqlite.count().await?, 1);
+        assert_eq!(sqlite.max_id().await?, 2);
 
         sqlite.drop().await?;
         sqlite.create().await?;
         sqlite.insert(&[1, 2, 3, 4]).await?;
-        println!("data: {:?}", sqlite.all(0).await?);
+        assert_eq!(
+            sqlite.all(0).await?,
+            vec![UpdateBinary {
+                id: 1,
+                blob: vec![1, 2, 3, 4]
+            }]
+        );
+        assert_eq!(sqlite.count().await?, 1);
 
         Ok(())
     }
