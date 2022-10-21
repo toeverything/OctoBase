c61b6d98942f925f385dbe9d539658c6b20d5594
austaras <austaras@outlook.com>
2022-10-21 14:27:33 +0800

    add empty struct

--- libs/jwst-ffi/binding.h
+++ libs/jwst-ffi/binding.h
@@ -1,3 +1,11 @@
+
+#ifndef JWST_FFI_H
+#define JWST_FFI_H
+typedef struct JWSTWorkspace {} JWSTWorkspace;
+typedef struct JWSTBlock {} JWSTBlock;
+typedef struct YTransaction {} YTransaction;        
+
+
 #include <cstdarg>
 #include <cstdint>
 #include <cstdlib>
@@ -31,48 +39,53 @@ struct BlockContent {
 
 extern "C" {
 
-Block *block_new(const Workspace *workspace,
-                 Transaction *trx,
-                 const char *block_id,
-                 const char *flavor,
-                 uint64_t operator_);
+JWSTBlock *block_new(const JWSTWorkspace *workspace,
+                     YTransaction *trx,
+                     const char *block_id,
+                     const char *flavor,
+                     uint64_t operator_);
 
-void block_destroy(Block *block);
+void block_destroy(JWSTBlock *block);
 
-uint64_t block_get_created(const Block *block);
+uint64_t block_get_created(const JWSTBlock *block);
 
-uint64_t block_get_updated(const Block *block);
+uint64_t block_get_updated(const JWSTBlock *block);
 
-char *block_get_flavor(const Block *block);
+char *block_get_flavor(const JWSTBlock *block);
 
-BlockChildren *block_get_children(const Block *block);
+BlockChildren *block_get_children(const JWSTBlock *block);
 
 void block_children_destroy(BlockChildren *children);
 
-BlockContent *block_get_content(const Block *block, const char *key);
+BlockContent *block_get_content(const JWSTBlock *block, const char *key);
 
-void block_set_content(Block *block, const char *key, Transaction *trx, BlockContent content);
+void block_set_content(JWSTBlock *block, const char *key, YTransaction *trx, BlockContent content);
 
 void block_content_destroy(BlockContent *content);
 
-Workspace *workspace_new(const char *id);
+JWSTWorkspace *workspace_new(const char *id);
 
-void workspace_destroy(Workspace *workspace);
+void workspace_destroy(JWSTWorkspace *workspace);
 
-Block *workspace_get_block(const Workspace *workspace, const char *block_id);
+JWSTBlock *workspace_get_block(const JWSTWorkspace *workspace, const char *block_id);
 
-Block *workspace_create_block(const Workspace *workspace, const char *block_id, const char *flavor);
+JWSTBlock *workspace_create_block(const JWSTWorkspace *workspace,
+                                  const char *block_id,
+                                  const char *flavor);
 
-bool workspace_remove_block(const Workspace *workspace, const char *block_id);
+bool workspace_remove_block(const JWSTWorkspace *workspace, const char *block_id);
 
-bool workspace_exists_block(const Workspace *workspace, const char *block_id);
+bool workspace_exists_block(const JWSTWorkspace *workspace, const char *block_id);
 
-Transaction *workspace_get_trx(Workspace *workspace);
+YTransaction *workspace_get_trx(JWSTWorkspace *workspace);
 
-Subscription<UpdateEvent> *workspace_observe(Workspace *workspace,
+Subscription<UpdateEvent> *workspace_observe(JWSTWorkspace *workspace,
                                              void *env,
-                                             void (*func)(void*, const Transaction*, const UpdateEvent*));
+                                             void (*func)(void*, const YTransaction*, const UpdateEvent*));
 
 void workspace_unobserve(Subscription<UpdateEvent> *subscription);
 
 } // extern "C"
+
+
+#endif            
--- libs/jwst-ffi/build.rs
+++ libs/jwst-ffi/build.rs
@@ -1,4 +1,4 @@
-use std::{env, path::PathBuf};
+use std::{collections::HashMap, env, path::PathBuf};
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
@@ -7,8 +7,39 @@ fn main() {
     )
     .join("binding.h");
 
+    let rename = {
+        let mut map = HashMap::new();
+
+        map.insert("Workspace".to_string(), "JWSTWorkspace".to_string());
+        map.insert("Block".to_string(), "JWSTBlock".to_string());
+        map.insert("Transaction".to_string(), "YTransaction".to_string());
+
+        map
+    };
+
     cbindgen::Builder::new()
         .with_crate(crate_dir)
+        .with_config(cbindgen::Config {
+            header: Some(String::from(
+                r#"
+#ifndef JWST_FFI_H
+#define JWST_FFI_H
+typedef struct JWSTWorkspace {} JWSTWorkspace;
+typedef struct JWSTBlock {} JWSTBlock;
+typedef struct YTransaction {} YTransaction;        
+"#,
+            )),
+            trailer: Some(String::from(
+                r#"
+#endif            
+"#,
+            )),
+            export: cbindgen::ExportConfig {
+                rename,
+                ..Default::default()
+            },
+            ..Default::default()
+        })
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(output);
