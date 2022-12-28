48601ca34bc38a615780407c986ee60845b2a80f
DarkSky <darksky2048@gmail.com>
2022-12-28 13:56:26 +0800

    fix: logger

--- libs/jwst-logger/src/logger.rs
+++ libs/jwst-logger/src/logger.rs
@@ -5,26 +5,23 @@ use tracing_subscriber::prelude::*;
 
 #[inline]
 pub fn init_logger() {
-    #[cfg(debug_assertions)]
-    {
-        let writer = stderr
-            .with_max_level(Level::WARN)
-            .or_else(stdout.with_max_level(if cfg!(debug_assertions) {
-                Level::DEBUG
-            } else {
-                Level::INFO
-            }));
+    let writer = stderr
+        .with_max_level(Level::WARN)
+        .or_else(stdout.with_max_level(if cfg!(debug_assertions) {
+            Level::DEBUG
+        } else {
+            Level::INFO
+        }));
 
-        tracing_subscriber::registry()
-            .with(
-                tracing_subscriber::fmt::layer()
-                    .map_writer(move |_| writer)
-                    .map_event_format(|e| JWSTFormatter {
-                        default: e.with_timer(LogTime),
-                    })
-                    .with_filter(GeneralFilter),
-            )
-            // .with(tracing_stackdriver::layer().with_filter(GeneralFilter))
-            .init();
-    }
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .map_writer(move |_| writer)
+                .map_event_format(|e| JWSTFormatter {
+                    default: e.with_timer(LogTime),
+                })
+                .with_filter(GeneralFilter),
+        )
+        // .with(tracing_stackdriver::layer().with_filter(GeneralFilter))
+        .init();
 }
