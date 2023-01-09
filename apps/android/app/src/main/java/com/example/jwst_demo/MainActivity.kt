634219d124fc2e2fdcf73ed746ee9579ba0ac48b
DarkSky <darksky2048@gmail.com>
2023-01-09 23:14:32 +0800

    feat: new syntax for create blocks

--- apps/android/app/src/main/java/com/example/jwst_demo/MainActivity.kt
+++ apps/android/app/src/main/java/com/example/jwst_demo/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.jwst_demo
 
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.toeverything.jwst.Storage
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,13 +18,22 @@ class MainActivity : AppCompatActivity() {
         val storage = Storage(database.absolutePath);
         val workspace = storage.getWorkspace("test")
 
-        workspace.withTrx { trx ->
-            val block = trx.create("a", "b");
-            block.set(trx, "a key", "a value");
-        };
-        val block = workspace.get("a").get();
-        val content = block.get("a key").get();
-        this.title = content as String;
-
+        workspace.get("a").ifPresentOrElse(
+            { block ->
+                // load the existing block on the second startup program.
+                val content = block.get("a key").get();
+                this.title = (content as String) + " exists";
+            },
+            {
+                // create a new block on the first startup program.
+                workspace.withTrx { trx ->
+                    val block = trx.create("a", "b");
+                    block.set(trx, "a key", "a value");
+                };
+                val block = workspace.get("a").get();
+                val content = block.get("a key").get();
+                this.title = content as String;
+            }
+        )
     }
 }
