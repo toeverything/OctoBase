851b473941061c540d4795e8068417b6ebd74d8d
DarkSky <darksky2048@gmail.com>
2023-01-18 14:08:46 +0800

    feat: first table for cloud database

--- libs/cloud-database/migration/src/lib.rs
+++ libs/cloud-database/migration/src/lib.rs
@@ -1,12 +1,14 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
+mod m20220101_000001_create_user_table;
+
+use async_trait::async_trait;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        vec![Box::new(m20220101_000001_create_user_table::Migration)]
     }
 }
