e10d07f5df76ee17eccbbad2cb9d19c154f6c842
DarkSky <darksky2048@gmail.com>
2023-01-30 18:56:47 +0800

    chore: cleanup codes

--- apps/pulsar/src/main.rs
+++ apps/pulsar/src/main.rs
@@ -11,11 +11,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // broadcast.listen("/ip4/0.0.0.0/tcp/1281/ws".parse()?)?;
     broadcast.connect("/ip4/127.0.0.1/tcp/1281/ws".parse()?)?;
 
-    // Read full lines from stdin
     let mut stdin = io::BufReader::new(io::stdin()).lines();
     println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
 
-    // Kick it off
     loop {
         tokio::select! {
             line = stdin.next_line() => {
@@ -25,28 +23,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 }
             },
             event = broadcast.next() => match event {
-                // SwarmEvent::Behaviour(WebSocketBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
-                //     for (peer_id, _multiaddr) in list {
-                //         println!("mDNS discovered a new peer: {peer_id}");
-                //         swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
-                //     }
-                // },
-                // SwarmEvent::Behaviour(WebSocketBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
-                //     for (peer_id, _multiaddr) in list {
-                //         println!("mDNS discover peer has expired: {peer_id}");
-                //         swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
-                //     }
-                // },
                 UpdateBroadcastEvent::Message {
-                    peer_id,
-                    id,
-                    message,
-                } => println!(
-                    "Got message: '{}' with id: {id} from peer: {peer_id}",
-                    String::from_utf8_lossy(&message.data),
-                ),
+                    peer_id, message, ..
+                } => {
+                    println!("{peer_id}: '{}'", String::from_utf8_lossy(&message.data));
+                }
                 UpdateBroadcastEvent::Other(other) => {
-                    println!("{other}");
+                    // println!("{other}");
                 }
             }
         }
