ec9b4788024c49475b6ef43897ce24c5cec5d407
xiaodong zuo <zuoxd02@gmail.com>
2023-02-02 14:20:09 +0800

    feat: add error infomation

--- apps/cloud/src/error_info.rs
+++ apps/cloud/src/error_info.rs
@@ -1,58 +1,73 @@
-use axum::response::{IntoResponse, Response};
+use axum::{
+    response::{IntoResponse, Response},
+    Json,
+};
 use http::StatusCode;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ErrorStatus {
+    message: String,
+}
+
+fn error_response(status: StatusCode, message: &str) -> Response {
+    (
+        status,
+        Json(ErrorStatus {
+            message: message.to_string(),
+        }),
+    )
+        .into_response()
+}
 
 pub fn not_modify_error() -> Response {
-    (StatusCode::NOT_MODIFIED, "The file is not modified").into_response()
+    error_response(StatusCode::NOT_MODIFIED, "The file is not modified")
 }
 
 pub fn not_found_error() -> Response {
-    (StatusCode::NOT_FOUND, "The file does not exist").into_response()
+    error_response(StatusCode::NOT_FOUND, "The file does not exist")
 }
 
 pub fn not_found_workspace_error(workspace_id: String) -> Response {
-    (
+    error_response(
         StatusCode::NOT_FOUND,
-        format!("Workspace({workspace_id:?}) not found."),
+        &format!("Workspace({workspace_id:?}) not found."),
     )
-        .into_response()
 }
 
 pub fn not_found_invitation_error() -> Response {
-    (
+    error_response(
         StatusCode::NOT_FOUND,
-        format!("Invitation link has expired."),
+        &format!("Invitation link has expired."),
     )
-        .into_response()
 }
 
 pub fn internal_server_error() -> Response {
-    (
+    error_response(
         StatusCode::INTERNAL_SERVER_ERROR,
         "Server error, please try again later.",
     )
-        .into_response()
 }
 
 pub fn payload_too_large_error() -> Response {
-    (
+    error_response(
         StatusCode::PAYLOAD_TOO_LARGE,
-        "Upload file size exceeds 10MB.",
+        "Upload file size exceeds 10MB",
     )
-        .into_response()
 }
 
 pub fn bad_request_error() -> Response {
-    (StatusCode::BAD_REQUEST, "Request parameter error.").into_response()
+    error_response(StatusCode::BAD_REQUEST, "Request parameter error.")
 }
 
 pub fn forbidden_error() -> Response {
-    (StatusCode::FORBIDDEN, "Sorry, you do not have permission.").into_response()
+    error_response(StatusCode::FORBIDDEN, "Sorry, you do not have permission.")
 }
 
 pub fn unauthorized_error() -> Response {
-    (StatusCode::UNAUTHORIZED, "Unauthorized").into_response()
+    error_response(StatusCode::UNAUTHORIZED, "Unauthorized.")
 }
 
 pub fn conflict_invitation_error() -> Response {
-    (StatusCode::CONFLICT, "Invitation failed.").into_response()
+    error_response(StatusCode::CONFLICT, "Invitation failed.")
 }
