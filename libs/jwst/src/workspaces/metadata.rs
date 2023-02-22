bd514d2b828d6ce135e7cd850430d387d2a9293d
JimmFly <yangjinfei001@gmail.com>
2023-02-22 15:13:35 +0800

    feat: add avatar workspace meta

--- libs/jwst/src/workspaces/metadata.rs
+++ libs/jwst/src/workspaces/metadata.rs
@@ -8,12 +8,14 @@ use yrs::{Map, MapRef, Transaction};
 #[derive(Debug, Clone, JsonSchema, Serialize, Deserialize)]
 pub struct WorkspaceMetadata {
     pub name: Option<String>,
+    pub avatar: Option<String>,
 }
 
 impl From<(&'_ Transaction<'_>, MapRef)> for WorkspaceMetadata {
     fn from((trx, map): (&Transaction, MapRef)) -> Self {
         Self {
             name: map.get(trx, "name").map(|s| s.to_string(trx)),
+            avatar: map.get(trx, "avatar").map(|s| s.to_string(trx)),
         }
     }
 }
@@ -23,6 +25,9 @@ impl From<WorkspaceMetadata> for Any {
         let mut map = HashMap::new();
         if let Some(name) = val.name {
             map.insert("name".to_owned(), name.into());
+        }
+        if let Some(avatar) = val.avatar {
+            map.insert("avatar".to_owned(), avatar.into());
         }
         Any::Map(map.into())
     }
