6ab2243b5e9017f1647e93ae4b2c1e7ce12839fc
Fangdun Tsai <fundon@users.noreply.github.com>
2023-02-23 01:46:39 +0800

    refactor: static sys variables (#203)
    * refactor: static sys variables
    
    * chore: rename to constants

---
+++ libs/jwst/src/constants.rs
@@ -0,0 +1,17 @@
+/// The sys constants.
+pub mod sys {
+    /// `sys:children`
+    pub const CHILDREN: &str = "sys:children";
+
+    /// `sys:created`
+    pub const CREATED: &str = "sys:created";
+
+    /// `sys:flavor`
+    pub const FLAVOR: &str = "sys:flavor";
+
+    /// `sys:parent`
+    pub const PARENT: &str = "sys:parent";
+
+    /// `sys:version`
+    pub const VERSION: &str = "sys:version";
+}
