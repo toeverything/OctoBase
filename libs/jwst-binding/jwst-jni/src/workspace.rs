2ca93dbfcf8802d427c4ad0f2d55dd60263d3b61
x1a0t <405028157@qq.com>
2023-03-27 17:30:33 +0800

    chore: lint style

--- libs/jwst-binding/jwst-jni/src/workspace.rs
+++ libs/jwst-binding/jwst-jni/src/workspace.rs
@@ -41,15 +41,16 @@ impl Workspace {
             .try_with_trx(|trx| on_trx.on_trx(WorkspaceTransaction(trx)))
             .is_some()
     }
+
     #[generate_interface]
     pub fn get_blocks_by_flavour(&self, flavour: &str) -> Vec<Block> {
         self.workspace.with_trx(|mut trx| {
             trx.get_blocks().get_blocks_by_flavour(&trx.trx, flavour)
                 .iter()
                 .map(|item| {
-                Block(item.clone())
-            })
-            .collect()
+                    Block(item.clone())
+                })
+                .collect()
         })
     }
 
