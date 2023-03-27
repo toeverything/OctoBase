51c54618f633c9cde325a5f205215e4c5dd29470
DarkSky <darksky2048@gmail.com>
2023-03-27 22:23:32 +0800

    fix: lib load

--- apps/homepage/components/TodoMVC.tsx
+++ apps/homepage/components/TodoMVC.tsx
@@ -1,29 +1,34 @@
-import { NoSsr } from '@mui/material'
 import { useHistory } from '@toeverything/jwt-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { SyncedTextBlock } from './SyncedTextBlock'
-import { nanoid } from './utils'
 
 export function TodoMVC() {
-	const [id] = useState(nanoid())
+	const [id, setId] = useState<string>()
 	const { undo, redo } = useHistory()
+
+	useEffect(() => {
+		const loadId = async () => {
+			const { nanoid } = await import('nanoid')
+			setId(nanoid())
+		}
+		loadId()
+		return () => setId(undefined)
+	}, [])
 
 	if (id) {
 		return (
-			<NoSsr>
-				<div style={{ display: 'flex', flexDirection: 'column' }}>
-					<span>{id}</span>
-					<div style={{ display: 'flex', flexDirection: 'row', margin: '0.5em' }}>
-						<SyncedTextBlock key={1} name="1" id={id} />
-						<SyncedTextBlock key={2} name="2" id={id} />
-					</div>
-					<div style={{ display: 'flex', flexDirection: 'row', margin: '0.5em' }}>
-						<button onClick={undo}>undo</button>
-						<button onClick={redo}>redo</button>
-					</div>
+			<div style={{ display: 'flex', flexDirection: 'column' }}>
+				<span>{id}</span>
+				<div style={{ display: 'flex', flexDirection: 'row', margin: '0.5em' }}>
+					<SyncedTextBlock key={1} name="1" id={id} />
+					<SyncedTextBlock key={2} name="2" id={id} />
 				</div>
-			</NoSsr>
+				<div style={{ display: 'flex', flexDirection: 'row', margin: '0.5em' }}>
+					<button onClick={undo}>undo</button>
+					<button onClick={redo}>redo</button>
+				</div>
+			</div>
 		)
 	}
 	return null
