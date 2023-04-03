dc7f6af12fd0c69708130dfc3d4c252df714bd4d
Fangdun Tsai <cfddream@gmail.com>
2023-04-04 05:15:08 +0800

    chore: comments

--- apps/keck/src/main.rs
+++ apps/keck/src/main.rs
@@ -9,7 +9,7 @@ const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[tokio::main]
 async fn main() {
-    // ignore load error
+    // ignore load error when missing env file
     let _ = dotenvy::dotenv();
     init_logger(PKG_NAME);
     jwst::print_versions(PKG_NAME, env!("CARGO_PKG_VERSION"));
