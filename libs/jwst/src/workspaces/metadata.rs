8638dcfb13f1924803a119dc142a3398ebe1165b
JimmFly <yangjinfei001@gmail.com>
2023-04-10 16:50:09 +0800

    feat: add avatar to workspace metadata

--- libs/jwst/src/workspaces/metadata.rs
+++ libs/jwst/src/workspaces/metadata.rs
@@ -10,7 +10,7 @@ pub const SEARCH_INDEX: &str = "search_index";
 #[derive(Debug, Clone, JsonSchema, Serialize, Deserialize)]
 pub struct WorkspaceMetadata {
     pub name: Option<String>,
-    // pub avatar: Option<String>,
+    pub avatar: Option<String>,
     pub search_index: Vec<String>,
 }
 
@@ -18,7 +18,7 @@ impl From<(&'_ Transaction<'_>, MapRef)> for WorkspaceMetadata {
     fn from((trx, map): (&Transaction, MapRef)) -> Self {
         Self {
             name: map.get(trx, "name").map(|s| s.to_string(trx)),
-            // avatar: map.get(trx, "avatar").map(|s| s.to_string(trx)),
+            avatar: map.get(trx, "avatar").map(|s| s.to_string(trx)),
             search_index: match map.get(trx, SEARCH_INDEX) {
                 Some(value) => serde_json::from_str::<Vec<String>>(&value.to_string(trx)).unwrap(),
                 None => vec!["title".to_string(), "text".to_string()],
@@ -33,9 +33,9 @@ impl From<WorkspaceMetadata> for Any {
         if let Some(name) = val.name {
             map.insert("name".to_owned(), name.into());
         }
-        // if let Some(avatar) = val.avatar {
-        //     map.insert("avatar".to_owned(), avatar.into());
-        // }
+        if let Some(avatar) = val.avatar {
+            map.insert("avatar".to_owned(), avatar.into());
+        }
         map.insert(SEARCH_INDEX.to_owned(), val.search_index.into());
         Any::Map(map.into())
     }
