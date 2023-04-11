fb8a784573a497525250ea5eebabf780f9a3d770
xiaodong zuo <zuoxd02@gmail.com>
2023-04-11 18:50:54 +0800

    fix: add tests of blob

--- apps/cloud/src/application/blob_service.rs
+++ apps/cloud/src/application/blob_service.rs
@@ -273,3 +273,273 @@ impl CloudBlobService {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::infrastructure::auth::get_claim_from_token;
+    use crate::{api::make_rest_route, context::Context};
+    use axum::extract::BodyStream;
+    use axum::extract::FromRequest;
+    use axum::http::Request;
+    use axum::{body::Body, http::StatusCode, Extension};
+    use axum_test_helper::TestClient;
+    use bytes::Bytes;
+    use cloud_database::CloudDatabase;
+    use futures::stream;
+    use serde_json::json;
+
+    async fn test_init() -> Arc<Context> {
+        let pool = CloudDatabase::init_pool("sqlite::memory:").await.unwrap();
+        let context = Context::new_test_client(pool).await;
+        Arc::new(context)
+    }
+
+    async fn test_user_login(client: &TestClient) -> String {
+        let body_data = json!({
+            "type": "DebugCreateUser",
+            "name": "my_username",
+            "avatar_url": "my_avatar_url",
+            "email": "my_email",
+            "password": "my_password",
+        });
+        let body_string = serde_json::to_string(&body_data).unwrap();
+        let resp = client
+            .post("/user/token")
+            .header("Content-Type", "application/json")
+            .body(body_string)
+            .send()
+            .await;
+        assert_eq!(resp.status(), StatusCode::OK);
+        // login user
+        let body_data = json!({
+            "type": "DebugLoginUser",
+            "email": "my_email",
+            "password": "my_password",
+        });
+        let body_string = serde_json::to_string(&body_data).unwrap();
+        let resp = client
+            .post("/user/token")
+            .header("Content-Type", "application/json")
+            .body(body_string)
+            .send()
+            .await;
+        assert_eq!(resp.status(), StatusCode::OK);
+        let resp_json: serde_json::Value = resp.json().await;
+        let access_token = resp_json["token"].as_str().unwrap().to_string();
+        access_token
+    }
+
+    fn test_user_id(access_token: String, ctx: &Arc<Context>) -> String {
+        let user = match get_claim_from_token(&access_token, &ctx.key.jwt_decode) {
+            Some(claims) => Some(claims.user.id),
+            None => None,
+        };
+        let user_id = user.unwrap_or("".to_string());
+        assert_ne!(user_id, "");
+        user_id
+    }
+
+    async fn test_workspace(client: &TestClient, access_token: String) -> String {
+        let test_data: Vec<u8> = (0..=255).collect();
+        let test_data_len = test_data.len();
+        let test_data_stream = stream::iter(
+            test_data
+                .into_iter()
+                .map(|byte| Ok::<_, std::io::Error>(Bytes::from(vec![byte]))),
+        );
+        let body_stream = Body::wrap_stream(test_data_stream.clone());
+        // create workspace
+        let resp = client
+            .post("/workspace")
+            .header("Content-Length", test_data_len.clone().to_string())
+            .header("authorization", format!("{}", access_token.clone()))
+            .body(body_stream)
+            .send()
+            .await;
+        assert_eq!(resp.status(), StatusCode::OK);
+        let resp_json: serde_json::Value = resp.json().await;
+        let workspace_id = resp_json["id"].as_str().unwrap().to_string();
+        workspace_id
+    }
+
+    async fn test_body_stream() -> BodyStream {
+        let test_data: Vec<u8> = (0..=255).collect();
+        let test_data_len = test_data.len();
+        let test_data_stream = stream::iter(
+            test_data
+                .into_iter()
+                .map(|byte| Ok::<_, std::io::Error>(Bytes::from(vec![byte]))),
+        );
+
+        // test too large
+        let res = Request::builder()
+            .header("content-length", test_data_len.to_string())
+            .body(Body::wrap_stream(test_data_stream.clone()))
+            .unwrap();
+        let body_stream = BodyStream::from_request(res, &"test".to_string())
+            .await
+            .unwrap();
+        body_stream
+    }
+
+    #[tokio::test]
+    async fn test_get_blob_in_workspace() {
+        // init data
+        let ctx = test_init().await;
+        let blob_service = &ctx.blob_service;
+        let app = make_rest_route(ctx.clone()).layer(Extension(ctx.clone()));
+        let client = TestClient::new(app);
+        let access_token = test_user_login(&client).await;
+        let user_id = test_user_id(access_token.clone(), &ctx);
+        let workspace_id = test_workspace(&client, access_token.clone()).await;
+
+        // upload blob
+        let body_stream = test_body_stream().await;
+        let upload_blob = blob_service
+            .upload_blob_in_workspace(
+                &ctx,
+                user_id.clone(),
+                workspace_id.clone(),
+                ContentLength(5 * 1024 as u64),
+                body_stream,
+            )
+            .await;
+        assert_eq!(upload_blob.is_ok(), true);
+
+        // test error
+        let mut header_map = HeaderMap::new();
+        header_map.insert(
+            "authorization",
+            HeaderValue::from_str(&format!("{}", access_token.clone())).unwrap(),
+        );
+        let err_test = blob_service
+            .get_blob_in_workspace(
+                &ctx,
+                workspace_id.clone(),
+                "other_blob".to_string(),
+                Method::GET,
+                header_map,
+            )
+            .await;
+        assert_eq!(err_test.is_err(), true);
+
+        // test success
+        let mut header_map = HeaderMap::new();
+        header_map.insert(
+            "authorization",
+            HeaderValue::from_str(&format!("{}", access_token.clone())).unwrap(),
+        );
+        let test_success = blob_service
+            .get_blob_in_workspace(
+                &ctx,
+                workspace_id.clone(),
+                upload_blob.ok().unwrap(),
+                Method::GET,
+                header_map,
+            )
+            .await;
+        assert_eq!(test_success.is_ok(), true);
+    }
+
+    #[tokio::test]
+    async fn test_upload_blob_in_workspace() {
+        // init data
+        let ctx = test_init().await;
+        let blob_service = &ctx.blob_service;
+        let app = make_rest_route(ctx.clone()).layer(Extension(ctx.clone()));
+        let client = TestClient::new(app);
+        let access_token = test_user_login(&client).await;
+        let user_id = test_user_id(access_token.clone(), &ctx);
+        let workspace_id = test_workspace(&client, access_token.clone()).await;
+
+        // test too large
+        let body_stream = test_body_stream().await;
+        let too_large = blob_service
+            .upload_blob_in_workspace(
+                &ctx,
+                user_id.clone(),
+                workspace_id.clone(),
+                ContentLength(20 * 1024 * 1024 as u64),
+                body_stream,
+            )
+            .await;
+        assert_eq!(too_large.is_err(), true);
+
+        // test too large
+        let body_stream = test_body_stream().await;
+        let too_large = blob_service
+            .upload_blob_in_workspace(
+                &ctx,
+                user_id.clone(),
+                workspace_id.clone(),
+                ContentLength(20 * 1024 * 1024 as u64),
+                body_stream,
+            )
+            .await;
+        assert_eq!(too_large.is_err(), true);
+
+        // test try other workspace
+        let body_stream = test_body_stream().await;
+        let try_other_workspace = blob_service
+            .upload_blob_in_workspace(
+                &ctx,
+                user_id.clone(),
+                "other_workspace_id".to_string(),
+                ContentLength(5 * 1024 as u64),
+                body_stream,
+            )
+            .await;
+        assert_eq!(try_other_workspace.is_err(), true);
+
+        // test upload success
+        let body_stream = test_body_stream().await;
+        let upload = blob_service
+            .upload_blob_in_workspace(
+                &ctx,
+                user_id.clone(),
+                workspace_id.clone(),
+                ContentLength(5 * 1024 as u64),
+                body_stream,
+            )
+            .await;
+        assert_eq!(upload.is_ok(), true);
+    }
+
+    #[tokio::test]
+    async fn test_get_user_resource_usage() {
+        // init data
+        let ctx = test_init().await;
+        let blob_service = &ctx.blob_service;
+        let app = make_rest_route(ctx.clone()).layer(Extension(ctx.clone()));
+        let client = TestClient::new(app);
+        let access_token = test_user_login(&client).await;
+        let user_id = test_user_id(access_token.clone(), &ctx);
+        let workspace_id = test_workspace(&client, access_token.clone()).await;
+
+        let usage = blob_service
+            .get_user_resource_usage(&ctx, user_id.clone())
+            .await;
+        assert_eq!(usage.is_ok(), true);
+        assert_eq!(usage.ok().unwrap().blob_usage.usage, 0 as u64);
+
+        // upload blob
+        let body_stream = test_body_stream().await;
+        let upload = blob_service
+            .upload_blob_in_workspace(
+                &ctx,
+                user_id.clone(),
+                workspace_id.clone(),
+                ContentLength(5 * 1024 as u64),
+                body_stream,
+            )
+            .await;
+        assert_eq!(upload.is_ok(), true);
+
+        let usage = blob_service
+            .get_user_resource_usage(&ctx, user_id.clone())
+            .await;
+        assert_eq!(usage.is_ok(), true);
+        assert_eq!(usage.ok().unwrap().blob_usage.usage, 256 as u64);
+    }
+}
