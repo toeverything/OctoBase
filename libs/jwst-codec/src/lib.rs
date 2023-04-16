60cc1e6845444aa211303cb8aba0ffb2b0ef0bba
DarkSky <darksky2048@gmail.com>
2023-04-17 00:05:53 +0800

    feat: add read structs part1

--- libs/jwst-codec/src/lib.rs
+++ libs/jwst-codec/src/lib.rs
@@ -1,4 +1,5 @@
 mod codec;
+mod update;
 
 pub use codec::{
     read_var_buffer, read_var_i64, read_var_string, read_var_u64, write_var_i64, write_var_u64,
---
+++ libs/jwst-codec/src/update.rs
@@ -0,0 +1,50 @@
+use nom::{bytes::complete::take, multi::count, IResult};
+
+use super::*;
+
+pub enum StructInfo {
+    GC,
+    Skip,
+}
+
+pub struct Structs {
+    client: u64,
+    clock: u64,
+    structs: Vec<StructInfo>,
+}
+
+fn parse_struct(input: &[u8]) -> IResult<&[u8], StructInfo> {
+    let (input, info) = take(1u8)(input)?;
+    let info = read_var_u64(info)?.1;
+    let first_5_bits = info & 0b11111;
+    match first_5_bits {
+        0 => Ok((input, StructInfo::GC)),
+        10 => Ok((input, StructInfo::Skip)),
+        _ => {
+            let cant_copy_parent_info = first_5_bits & 0b11000000;
+            todo!()
+        }
+    }
+}
+
+fn parse_structs(input: &[u8]) -> IResult<&[u8], Structs> {
+    let (input, num_of_structs) = read_var_u64(input)?;
+    let (input, client) = read_var_u64(input)?;
+    let (input, clock) = read_var_u64(input)?;
+    let (input, structs) = count(parse_struct, num_of_structs as usize)(input)?;
+    Ok((
+        input,
+        Structs {
+            client,
+            clock,
+            structs,
+        },
+    ))
+}
+
+pub fn read_client_struct_refs(input: &[u8]) -> IResult<&[u8], Vec<Structs>> {
+    let (input, num_of_updates) = read_var_u64(input)?;
+    let (tail, updates) = count(parse_structs, num_of_updates as usize)(input)?;
+
+    Ok((tail, updates))
+}
