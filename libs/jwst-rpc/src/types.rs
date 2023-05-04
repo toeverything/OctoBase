ae4cf0e49df821d9516080c4193928b42f710b3b
DarkSky <25152247+darkskygit@users.noreply.github.com>
2023-05-04 17:36:13 +0800

    chore: bump deps version & add feature split (#415)

--- libs/jwst-rpc/src/types.rs
+++ libs/jwst-rpc/src/types.rs
@@ -1,14 +1,17 @@
 use thiserror::Error;
+#[cfg(feature = "websocket")]
 use tokio_tungstenite::tungstenite;
 
 #[derive(Debug, Error)]
 pub enum JwstRpcError {
+    #[cfg(feature = "websocket")]
     #[error("failed to connect websocket")]
     WebsocketConnect(#[from] tungstenite::Error),
     #[error("jwst error")]
     Jwst(#[from] jwst::JwstError),
+    #[cfg(feature = "websocket")]
     #[error("failed to parse url")]
     UrlParse(#[from] url::ParseError),
 }
 
-pub type JwstRPCResult<T> = Result<T, JwstRpcError>;
+pub type JwstRpcResult<T> = Result<T, JwstRpcError>;
