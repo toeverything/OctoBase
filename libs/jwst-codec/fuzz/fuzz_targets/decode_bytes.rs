13932ebb57aaf70299a67faebb7003dad26add65
DarkSky <darksky2048@gmail.com>
2023-05-10 11:14:51 +0800

    chore: fix lint

--- libs/jwst-codec/fuzz/fuzz_targets/decode_bytes.rs
+++ libs/jwst-codec/fuzz/fuzz_targets/decode_bytes.rs
@@ -4,8 +4,8 @@ use jwst_codec::{read_var_buffer, read_var_i64, read_var_string, read_var_u64};
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: Vec<u8>| {
-    read_var_i64(&data);
-    read_var_u64(&data);
-    read_var_buffer(&data);
-    read_var_string(&data);
+    let _ = read_var_i64(&data);
+    let _ = read_var_u64(&data);
+    let _ = read_var_buffer(&data);
+    let _ = read_var_string(&data);
 });
