77991480935f98905955970115c3347aeb994606
x1a0t <405028157@qq.com>
2023-05-19 11:23:41 +0800

    chore: wrap transaction with retry (#424)
    * chore: wrap transaction with retry
    
    * chore: bump jni
    
    * ci: bump to gradle-build-action@v2.4.2

--- libs/jwst-storage/src/storage/mod.rs
+++ libs/jwst-storage/src/storage/mod.rs
@@ -146,7 +146,7 @@ impl JwstStorage {
                         };
                         Some(update)
                     } else {
-                        workspace.sync_migration(50).ok()
+                        workspace.sync_migration().ok()
                     };
 
                     let Some(update) = update else {
