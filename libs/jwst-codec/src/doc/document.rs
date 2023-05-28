6a986c602409c515ae64669992973ee9cbe74e78
x1a0t <405028157@qq.com>
2023-05-29 00:33:14 +0800

    feat: doc encoding diffing state vector (#442)
    * feat: complete doc encoding
    
    * fix: unit test
    
    * fix: remove unwrap

--- libs/jwst-codec/src/doc/document.rs
+++ libs/jwst-codec/src/doc/document.rs
@@ -31,6 +31,19 @@ impl Default for Doc {
 }
 
 impl Doc {
+    pub fn new_from_binary(binary: Vec<u8>) -> JwstCodecResult<Self> {
+        let mut doc = Doc::default();
+        doc.apply_update_from_binary(binary)?;
+        Ok(doc)
+    }
+
+    pub fn apply_update_from_binary(&mut self, update: Vec<u8>) -> JwstCodecResult {
+        let mut decoder = RawDecoder::new(update);
+        let update = Update::read(&mut decoder)?;
+        self.apply_update(update)?;
+        Ok(())
+    }
+
     pub fn apply_update(&mut self, mut update: Update) -> JwstCodecResult {
         let mut retry = false;
         loop {
@@ -417,11 +430,77 @@ impl Doc {
         })
     }
 
-    pub fn encode_update_v1(self) -> JwstCodecResult<Vec<u8>> {
-        let store = self.store;
+    pub fn encode_update_v1(&self) -> JwstCodecResult<Vec<u8>> {
+        self.encode_state_as_update_v1(&StateVector::default())
+    }
+
+    pub fn encode_state_as_update_v1(&self, sv: &StateVector) -> JwstCodecResult<Vec<u8>> {
+        let store = &self.store;
         let mut encoder = RawEncoder::default();
-        store.encode_with_state_vector(&StateVector::default(), &mut encoder)?;
+        store.encode_with_state_vector(sv, &mut encoder)?;
 
         Ok(encoder.into_inner())
+    }
+
+    pub fn get_state_vector(&self) -> StateVector {
+        self.store.get_state_vector()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use yrs::{Array, Map, Transact};
+
+    #[test]
+    fn double_run_test_with_yrs_basic() {
+        let yrs_doc = yrs::Doc::new();
+
+        let map = yrs_doc.get_or_insert_map("abc");
+        let mut trx = yrs_doc.transact_mut();
+        map.insert(&mut trx, "a", 1).unwrap();
+
+        let binary_from_yrs = trx.encode_update_v1().unwrap();
+        let doc = Doc::new_from_binary(binary_from_yrs.clone()).unwrap();
+        let binary = doc.encode_update_v1().unwrap();
+
+        assert_eq!(binary_from_yrs, binary);
+    }
+
+    #[test]
+    fn test_encode_state_as_update() {
+        let yrs_doc = yrs::Doc::new();
+
+        let map = yrs_doc.get_or_insert_map("abc");
+        let mut trx = yrs_doc.transact_mut();
+        map.insert(&mut trx, "a", 1).unwrap();
+        let binary_from_yrs = trx.encode_update_v1().unwrap();
+
+        let yrs_doc_new = yrs::Doc::new();
+        let array = yrs_doc_new.get_or_insert_array("array");
+        let mut trx = yrs_doc_new.transact_mut();
+        array.insert(&mut trx, 0, "array_value").unwrap();
+        let binary_from_yrs_new = trx.encode_update_v1().unwrap();
+
+        let mut doc = Doc::new_from_binary(binary_from_yrs.clone()).unwrap();
+        let mut doc_new = Doc::new_from_binary(binary_from_yrs_new.clone()).unwrap();
+
+        let diff_update = doc_new
+            .encode_state_as_update_v1(&doc.get_state_vector())
+            .unwrap();
+
+        let diff_update_reverse = doc
+            .encode_state_as_update_v1(&doc_new.get_state_vector())
+            .unwrap();
+
+        doc.apply_update_from_binary(diff_update).unwrap();
+        doc_new
+            .apply_update_from_binary(diff_update_reverse)
+            .unwrap();
+
+        assert_eq!(
+            doc.encode_update_v1().unwrap(),
+            doc_new.encode_update_v1().unwrap()
+        );
     }
 }
