e1706d77a02f9ea0b4461360834256159c5768a7
DarkSky <darksky2048@gmail.com>
2023-06-30 18:05:59 +0800

    fix: test

--- libs/jwst-codec/src/sync.rs
+++ libs/jwst-codec/src/sync.rs
@@ -5,8 +5,10 @@
 // };
 
 // #[cfg(not(loom))]
-#[allow(unused_imports)]
 pub(crate) use std::sync::{
-    atomic::{AtomicU8, AtomicUsize, Ordering},
+    atomic::{AtomicU8, Ordering},
     Arc, RwLock, RwLockReadGuard, RwLockWriteGuard, Weak,
 };
+
+#[cfg(test)]
+pub(crate) use std::sync::{atomic::AtomicUsize, Mutex, MutexGuard};
