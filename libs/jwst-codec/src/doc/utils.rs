48f04dea3e0e54b59244af2d28171f8ab4dd2eff
DarkSky <25152247+darkskygit@users.noreply.github.com>
2023-09-08 17:31:39 +0800

    feat: enable gc in migrate update (#534)
    * feat: enable gc in migrate update
    
    * chore: bump jni version

--- libs/jwst-codec/src/doc/utils.rs
+++ libs/jwst-codec/src/doc/utils.rs
@@ -38,6 +38,14 @@ pub fn decode_maybe_update_with_guid(binary: Vec<u8>) -> (String, Vec<u8>) {
     }
 }
 
+pub fn encode_awareness_as_message(awareness: AwarenessStates) -> JwstCodecResult<Vec<u8>> {
+    let mut buffer = Vec::new();
+    write_sync_message(&mut buffer, &SyncMessage::Awareness(awareness))
+        .map_err(|e| JwstCodecError::InvalidWriteBuffer(e.to_string()))?;
+
+    Ok(buffer)
+}
+
 pub fn encode_update_as_message(update: Vec<u8>) -> JwstCodecResult<Vec<u8>> {
     let mut buffer = Vec::new();
     write_sync_message(&mut buffer, &SyncMessage::Doc(DocMessage::Update(update)))
